4364cf166273ad366495326a9d0165b1c91573be
Keiu Harada <keiu.harada@gmailc.om>
2015-05-22 14:57:08 +0900

    fix Installer for vim plugins

--- Installer.sh
+++ Installer.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim/conf.d/neocomplete.vimrc ~/.vim/conf.d/neocomplete.vimrc
+ln -s ~/dotfiles/.vim/conf.d/vimproc.vimrc ~/.vim/conf.d/vimproc.vimrc
 
